df8ed59a585b149510d99fa5bc54b834bf1ad54b
ald-ahmed <ald.ahmed@yahoo.com>
2016-11-10 19:38:01 -0800

    removed scrolling plugin, enhanced sizes for usability, and added delete to table

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-/*
-
-Scrolling plugin
-
-*/
-//--------------------------------------------------------
-$('.main').onepage_scroll({
-    sectionContainer: 'section',
-    easing: 'ease-in-out',
-    animationTime: 1200,
-    pagination: false,
-    updateURL: false,
-    beforeMove: function (index) {},
-
-    afterMove: function (index) {},
-
-    loop: false,
-    responsiveFallback: false,
-    hybrid: true,
-    scrollOverflow: true,
-    direction: 'vertical',
-  });
-
-
 
 /*
 
@@ -30,13 +6,13 @@ name of folder where pics are located
 */
 //--------------------------------------------------------
 var directory = 'data';
-
-
 /*
 
 call api and get chips of users (right below where you put email)
 
+
 */
+
 //--------------------------------------------------------
 
 $.ajax({
@@ -64,6 +40,7 @@ Animations to fade in and fade out
 //--------------------------------------------------------
 
 function fadeIn(element) {
+  $(element).css('display', 'block');
   $(element).addClass('animated');
   $(element).css('visibility', 'visible');
   $(element).addClass('fadeIn');
@@ -73,6 +50,8 @@ function fadeOut(element) {
   $(element).addClass('animated');
   $(element).addClass('fadeOut');
   $(element).css('visibility', 'hidden');
+  $(element).css('display', 'none');
+
 }
 
 
@@ -119,9 +98,6 @@ $('body').on('click', '.chip', function () {
     $('.loginEmail').val($(this).text());
   });
 
-
-
-
 /*
 
 we display the annotation hub, refresh the most recent submission table, and call populateMediaAndOptions
@@ -134,11 +110,14 @@ function setupAnnotateHub() {
 
   refreshTable();
   populateMediaAndOptions();
-  fadeIn('.annotate');
 
   $(document).ajaxStop(function () {
-      $('.main').moveDown();
-  });
+
+      $('.login').fadeOut('fast', function () {
+        fadeIn('.annotate');
+      });
+
+    });
 
 }
 
@@ -151,20 +130,20 @@ use this function to load the feature checkboxes and run checkOut(); to get the 
 //--------------------------------------------------------
 function populateMediaAndOptions() {
 
-    $.ajax({
-        type: 'GET',
-        url: 'http://kitelore.com/api.php',
-        data: 'request=featureList',
-        success: function(data) {
-            $('.optionBucket').html(data);
+  $.ajax({
+      type: 'GET',
+      url: 'http://kitelore.com/api.php',
+      data: 'request=featureList',
+      success: function (data) {
+          $('.optionBucket').html(data);
         },
 
-        error: function(xhr, type, exception) {
-            // if ajax fails display error alert
-            console.log('ajax error response type ' + type);
+      error: function (xhr, type, exception) {
+          // if ajax fails display error alert
+          console.log('ajax error response type ' + type);
         },
     });
-    checkOut();
+  checkOut();
 
 }
 
@@ -183,25 +162,25 @@ var checkedOut;
 
 function checkOut() {
 
-    $.ajax({
-        type: 'GET',
-        url: 'http://kitelore.com/api.php',
-        data: 'request=checkOut',
-        success: function(data) {
-            if (data === '') {
-                data = 0;
-            }
+  $.ajax({
+      type: 'GET',
+      url: 'http://kitelore.com/api.php',
+      data: 'request=checkOut',
+      success: function (data) {
+          if (data === '') {
+            data = 0;
+          }
 
-            checkedOut = parseInt(data) + 1;
-            var progressBarValue = ((checkedOut * 100 / 10) + '%');
-            $('.img-subject').attr('src', directory + '/' + checkedOut + '.jpg');
-            $('.progress-bar').css('width', progressBarValue);
+          checkedOut = parseInt(data) + 1;
+          var progressBarValue = ((checkedOut * 100 / 10) + '%');
+          $('.img-subject').attr('src', directory + '/' + checkedOut + '.jpg');
+          $('.progress-bar').css('width', progressBarValue);
 
         },
 
-        error: function(xhr, type, exception) {
-            // if ajax fails display error alert
-            console.log('ajax error response type ' + type);
+      error: function (xhr, type, exception) {
+          // if ajax fails display error alert
+          console.log('ajax error response type ' + type);
         },
     });
 
@@ -218,18 +197,18 @@ when hovering over the image, show live postion, and when clicked, capture it
 */
 //--------------------------------------------------------
 
-$('.img-subject').click(function(e) {
+$('.img-subject').click(function (e) {
     var parentOffset = $(this).offset();
     $('.dataX').html((((e.pageX - parentOffset.left) / $(this).width()) * 100).toFixed(3) + ' %');
     $('.dataY').html((((e.pageY - parentOffset.top) / $(this).height()) * 100).toFixed(3) + ' %');
-});
+  });
 
-$('.img-subject').mousemove(function(e) {
+$('.img-subject').mousemove(function (e) {
     var parentOffset = $(this).offset();
     var relativeXPosition = (((e.pageX - parentOffset.left) / $(this).width()) * 100).toFixed(3);
     var relativeYPosition = (((e.pageY - parentOffset.top) / $(this).height()) * 100).toFixed(3);
     $('.livePosition').html(relativeXPosition + ' %' + ', ' + relativeYPosition + ' %');
-});
+  });
 
 
 
@@ -242,71 +221,87 @@ $('.img-subject').mousemove(function(e) {
 //--------------------------------------------------------
 
 function refreshTable() {
-    $.ajax({
-        type: 'GET',
-        url: 'http://kitelore.com/api.php',
-        data: 'request=getTable',
-        success: function(data) {
-            $('.table-bordered').html(data);
+  $.ajax({
+      type: 'GET',
+      url: 'http://kitelore.com/api.php',
+      data: 'request=getTable',
+      success: function (data) {
+          $('.table-bordered').html(data);
         },
 
-        error: function(xhr, type, exception) {
-            // if ajax fails display error alert
-            console.log('ajax error response type ' + type);
+      error: function (xhr, type, exception) {
+          // if ajax fails display error alert
+          console.log('ajax error response type ' + type);
         },
     });
 }
 
+$('body').on('click', '.deleteRow', function () {
 
+      $.ajax({
+          type: 'GET',
+          url: 'http://kitelore.com/api.php',
+          data: 'request=delete&data=' + this.id,
+          success: function (data) {
+              refreshTable();
+            },
+
+          error: function (xhr, type, exception) {
+              // if ajax fails display error alert
+              console.log('ajax error response type ' + type);
+            },
+        });
+
+    });
 
 
 
 /*
 
-  submit to database by aggreating the input/div values into dataComplete.
+  submit to database by aggregating the input/div values into dataComplete.
   Push to database, and call refreshTable and populateMediaAndOptions
 
 
   */
 //--------------------------------------------------------
 
-$('.annotateSubmit').click(function(event) {
+$('.annotateSubmit').click(function (event) {
 
     var dataComplete = [];
 
     dataComplete.push({
-        user: $('.loginEmail').val()
-    });
+        user: $('.loginEmail').val(),
+      });
     dataComplete.push({
         x: $('.dataX').text(),
-        y: $('.dataY').text()
-    });
+        y: $('.dataY').text(),
+      });
     dataComplete.push({
-        pic_id: checkedOut
-    });
+        pic_id: checkedOut,
+      });
 
-    $('.optionBucket input[type="checkbox"]').each(function(index, ez) {
+    $('.optionBucket input[type="checkbox"]').each(function (index, ez) {
         var element = $(this).attr('val');
         var data = $(this).is(':checked');
         dataComplete.push({
             feature: element,
-            status: data * 1
-        });
-    });
+            status: data * 1,
+          });
+      });
 
     $.ajax({
         type: 'POST',
         url: 'http://kitelore.com/api.php',
         data: 'request=submitAnnotation&data=' + JSON.stringify(dataComplete),
-        success: function(data) {
+        success: function (data) {
             refreshTable();
             populateMediaAndOptions();
-        },
+          },
 
-        error: function(xhr, type, exception) {
+        error: function (xhr, type, exception) {
             // if ajax fails display error alert
             console.log('ajax error response type ' + type);
-        },
-    });
+          },
+      });
 
-});
+  });
